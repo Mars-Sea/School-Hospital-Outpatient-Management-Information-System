0781bf0de6c0481f253b010dd3e5333ce1c40f93
mars_sea <1195751600@qq.com>
2020-12-22 15:47:30 +0800

    first commit

---
+++ src/main/java/cn/edu/cdtu/SHOMIS/ShomisApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cdtu.SHOMIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShomisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShomisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/cdtu/SHOMIS/ShomisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cdtu.SHOMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShomisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

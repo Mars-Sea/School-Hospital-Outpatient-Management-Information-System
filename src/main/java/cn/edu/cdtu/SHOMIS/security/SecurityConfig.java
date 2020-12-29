758127496e12b2e781e1581bf5afbaedf38cd876
liwuxin <1184144908@qq.com>
2020-12-29 11:33:47 +0800

    页面引入bootstrap

--- src/main/java/cn/edu/cdtu/SHOMIS/security/SecurityConfig.java
+++ src/main/java/cn/edu/cdtu/SHOMIS/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -30,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.passwordEncoder(NoOpPasswordEncoder.getInstance())
 				.withUser("admin").password("123456").roles("ADMIN");
 	}
+
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
